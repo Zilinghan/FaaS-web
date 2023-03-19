de09496c2bd414fcf924155881d1e81c24d5b045
Zilinghan <zl52@illinois.edu>
2023-03-19 01:17:53 -0500

    move config out of 'if'

--- run_portal.py
+++ run_portal.py
@@ -6,8 +6,8 @@ UPLOAD_FOLDER = './data/uploads'
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024 # The maximum file size is set to be 8MB
+
 if __name__ == '__main__':
-    # app.debug = False
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-    app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024 # The maximum file size is set to be 8MB
-    app.run(host='localhost', port=8000, ssl_context=('./ssl/cert.pem', './ssl/key.pem'))
+    app.run(host='0.0.0.0', port=8000, ssl_context=('./ssl/cert.pem', './ssl/key.pem'))
