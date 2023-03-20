bfaadf491df4cfe8c29d51abbeed1c8cf3fae271
Zilinghan <zl52@illinois.edu>
2023-03-20 10:31:49 -0500

    add doc on setting up endpoint

---
+++ docments/config-gpu.py
@@ -0,0 +1,64 @@
+"""
+For NCSA Delta Cluster: GPU
+"""
+import socket
+from parsl.launchers import SrunLauncher
+from parsl.providers import SlurmProvider
+
+from funcx_endpoint.endpoint.utils.config import Config
+from funcx_endpoint.executors import HighThroughputExecutor
+
+# fmt: off
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+
+# PLEASE UPDATE user_opts BEFORE USE
+user_opts = {
+    'delta': {
+        'worker_init': 'module load anaconda3_gpu; conda init bash; source ~/.bashrc; conda activate funcx',
+        'scheduler_options': '#SBATCH --mail-user=xxx@illinois.edu\n#SBATCH --mail-type=ALL\n#SBATCH --gpus-per-node=1\n#SBATCH --gpu-bind=none\n#SBATCH --constraint=projects',
+    }
+}
+
+config = Config(
+    executors=[
+        HighThroughputExecutor(
+            max_workers_per_node=2,
+            worker_debug=True,
+            address=ip_address,
+            provider=SlurmProvider(
+                account='bbke-delta-gpu',
+                partition='gpuA100x4',
+                launcher=SrunLauncher(),
+                # string to prepend to #SBATCH blocks in the submit
+                # script to the scheduler eg: '#SBATCH --constraint=knl,quad,cache'
+                scheduler_options=user_opts['delta']['scheduler_options'],
+
+                # Command to be run before starting a worker, such as:
+                # 'module load Anaconda; source activate parsl_env'.
+                worker_init=user_opts['delta']['worker_init'],
+
+                # Scale between 0-1 blocks with 2 nodes per block
+                nodes_per_block=2,
+                init_blocks=1,
+                min_blocks=1,
+                max_blocks=1,
+
+                # Hold blocks for 30 minutes
+                walltime='00:30:00'
+            ),
+        )
+    ],
+)
+
+# For now, visible_to must be a list of URNs for globus auth users or groups, e.g.:
+# urn:globus:auth:identity:{user_uuid}
+# urn:globus:groups:id:{group_uuid}
+meta = {
+    "name": '<ENDPOINT_NAME>',
+    "description": "",
+    "organization": "",
+    "department": "",
+    "public": False,
+    "visible_to": [],
+}
---
+++ docments/funcx.md
@@ -0,0 +1,57 @@
+# How to setup funcx endpoint on Delta Cluster @ NCSA
+
+## Installation
+0.  ssh to the login node of your cluster and load the anaconda module. For Delta, you can use the following command if you want to use GPU
+
+    ```
+    module load anaconda3_gpu
+    ```
+    or this fow CPU
+    ```
+    module load anaconda3_cpu
+    ```
+1. Create a virtual environment using `conda`
+
+    ```
+    conda create -n funcx python=3.8
+    conda activate funcx
+    ```
+2. Clone this repository, and install dependencies.
+
+    ```
+    git clone https://github.com/Zilinghan/FL-as-a-Service.git FaaS
+    cd FaaS
+    git checkout funcx
+    pip install -r requirements.txt
+    pip install -e .
+    ```
+
+
+## FuncX Endpoint Config
+3. Setup funcX endpoint. Please add your own `<ENDPOINT_NAME>` such as `delta-cpu-01`. 
+
+    You might be required to login with [Globus](https://app.globus.org), following the prompt instructions and finish the authentication steps.
+
+    ```
+    funcx-endpoint configure <ENDPOINT_NAME>
+    ```
+
+4. Confiugre the endpoint by editting the file `~/.funcx/<ENDPOINT_NAME>/config.py`. You can refer to these two sample configurations: [[CPU](config-cpu.py)] [[GPU](config-gpu.py)]. Please pay attention to the following points. More information of the configuration for endpoint can be found [here](https://funcx.readthedocs.io/en/latest/endpoints.html).
+
+    (1) Put whatever cmds you want to run before starting a worker into `'worker_init'` part.
+
+    (2) Put whatever cmds you want to run with `#SBATCH` into the `'scheduler_options'` part, e.g., change the `--mail-user` to your email address.
+    
+    (3) Replace the `<ENDPOINT_NAME>` to your created name.
+
+5. Start the funcX endpoint. The follwoing command will allocate resources you required from the `config.py` file above. [**Note**: Whenever you modify the `config.py`, you need to first run `funcx-endpoint stop <ENDPOINT_NAME>` and then re-start it to have the changes make effect.]
+
+    ```
+    funcx-endpoint start <ENDPOINT_NAME>
+    ```
+
+6. Get you endpoint id.
+    ```
+    funcx-endpoint list
+    ```
+
